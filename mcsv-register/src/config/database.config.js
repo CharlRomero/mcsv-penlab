6af71dd6a77187a25a6361ca71d4a3b6e1c367cc
CharlRomero <romeokarlos12@gmail.com>
2024-09-07 01:31:49 -0500

    Deleted token pool

--- mcsv-register/src/config/database.config.js
+++ mcsv-register/src/config/database.config.js
@@ -5,10 +5,6 @@ import {
   DB_USER,
   DB_PASSWORD_USER,
   DATABASE_USER,
-  DB_HOST_TOKEN,
-  DB_USER_TOKEN,
-  DB_PASSWORD_TOKEN,
-  DATABASE_TOKEN,
 } from "./env.config.js";
 
 export const userPool = new createPool({
@@ -17,16 +13,6 @@ export const userPool = new createPool({
   user: DB_USER,
   password: DB_PASSWORD_USER,
   database: DATABASE_USER,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-
-export const tokenPool = new createPool({
-  host: DB_HOST_TOKEN,
-  user: DB_USER_TOKEN,
-  password: DB_PASSWORD_TOKEN,
-  database: DATABASE_TOKEN,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
