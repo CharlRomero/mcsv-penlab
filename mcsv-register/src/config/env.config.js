af93d132497bcbf7c91a03890734a4eb1c645a6a
CharlRomero <romeokarlos12@gmail.com>
2024-09-07 01:32:16 -0500

    Deleted token env

--- mcsv-register/src/config/env.config.js
+++ mcsv-register/src/config/env.config.js
@@ -9,9 +9,3 @@ export const DB_PORT_USER = process.env.DB_PORT_USER;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD_USER = process.env.DB_PASSWORD_USER;
 export const DATABASE_USER = process.env.DATABASE_USER;
-//DB Token
-export const DB_HOST_TOKEN = process.env.DB_HOST_TOKEN;
-export const DB_PORT_TOKEN = process.env.DB_PORT_TOKEN;
-export const DB_USER_TOKEN = process.env.DB_USER_TOKEN;
-export const DB_PASSWORD_TOKEN = process.env.DB_PASSWORD_TOKEN;
-export const DATABASE_TOKEN = process.env.DATABASE_TOKEN;
