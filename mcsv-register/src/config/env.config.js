a963b287517e87859f766507e5d569decfc17e99
CharlRomero <romeokarlos12@gmail.com>
2024-09-07 01:17:02 -0500

    Added db token config

--- mcsv-register/src/config/env.config.js
+++ mcsv-register/src/config/env.config.js
@@ -1,9 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+//Microservice port
 export const PORT = process.env.PORT;
-export const HOST = process.env.HOST;
-export const DB_PORT = process.env.DB_PORT;
+//DB User
+export const DB_HOST_USER = process.env.DB_HOST_USER;
+export const DB_PORT_USER = process.env.DB_PORT_USER;
 export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DATABASE = process.env.DATABASE;
+export const DB_PASSWORD_USER = process.env.DB_PASSWORD_USER;
+export const DATABASE_USER = process.env.DATABASE_USER;
+//DB Token
+export const DB_HOST_TOKEN = process.env.DB_HOST_TOKEN;
+export const DB_PORT_TOKEN = process.env.DB_PORT_TOKEN;
+export const DB_USER_TOKEN = process.env.DB_USER_TOKEN;
+export const DB_PASSWORD_TOKEN = process.env.DB_PASSWORD_TOKEN;
+export const DATABASE_TOKEN = process.env.DATABASE_TOKEN;
