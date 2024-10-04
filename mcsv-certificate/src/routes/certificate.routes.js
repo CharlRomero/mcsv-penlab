bce9d27358f27c9035bd576b15213f84cafcd791
Bryan Adrian Iza Oña <65301459+Baio99@users.noreply.github.com>
2024-10-04 15:30:08 -0500

    Protection routes with token

--- mcsv-certificate/src/routes/certificate.routes.js
+++ mcsv-certificate/src/routes/certificate.routes.js
@@ -1,8 +1,10 @@
-import { Router } from "express";
-import { generateCertificateHandler } from "../controller/certificate.controller.js";
+// certificate.routes.js
+import { Router } from 'express';
+import { generateCertificateHandler } from '../controller/certificate.controller.js';
+import { authenticateToken } from '../utils/auth.middleware.js'; // Importa el middleware
 
 const router = Router();
 
-router.post("/generate", generateCertificateHandler);
+router.post('/generate', authenticateToken, generateCertificateHandler); // Protege la ruta
 
 export default router;
