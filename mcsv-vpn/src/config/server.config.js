f764788a1f27d97a8e946763cfbfa75c298b024c
Bryan Adrian Iza Oña <65301459+Baio99@users.noreply.github.com>
2024-10-16 12:21:25 -0500

    Template certificate

--- mcsv-vpn/src/config/server.config.js
+++ mcsv-vpn/src/config/server.config.js
@@ -5,7 +5,7 @@ import vpnRoutes from "../routes/index.routes.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://192.168.10.239:8082", credentials: true }));
+app.use(cors({ origin: "http://192.168.10.101:8080", credentials: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
